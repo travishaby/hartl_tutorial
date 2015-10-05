bec5c8002d81946a9b9121d607afa8e9d44094c5
Travis Haby <travishaby@gmail.com>
2015-10-05 16:38:51 -0600

    added more feature tests for navbar links also changed minitest syntac from old(?) style given in the tutorial to the new(?) style used at Turing

--- test/features/click_links_on_landing_page_test.rb
+++ test/features/click_links_on_landing_page_test.rb
@@ -1,19 +1,34 @@
 require "test_helper"
 
 class ClickLinksOnLandingPageTest < Capybara::Rails::TestCase
-  test "sanity" do
+  def test_sanity
     visit root_path
     assert page.has_content?("Sample App")
   end
 
-  test "clicking navbar links" do
+  def test_clicking_navbar_links
     visit root_path
     within("#navbar") do
       click_link "About"
     end
 
     assert_equal current_path, about_path
+    assert_equal "About | Ruby on Rails Tutorial Sample App", page.title
     message = "This is the sample application for the tutorial."
     assert page.has_content?(message)
+
+    visit root_path
+    within("#navbar") do
+      click_link "Help"
+    end
+
+    assert_equal current_path, help_path
+    assert_equal "Help | Ruby on Rails Tutorial Sample App", page.title
+    message = "Help"
+    assert page.has_content?(message)
+  end
+
+  def test_something_else
+
   end
 end
