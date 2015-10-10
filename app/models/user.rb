24d72cfb4627158bd67bfdba3d9d37afd006ab1d
Travis Haby <travishaby@gmail.com>
2015-10-09 18:26:14 -0700

    changed User model methods to be contained in self namespace instead of explicitly calling User.method

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,16 +13,18 @@ class User < ActiveRecord::Base
   def sanitize_email
     self.email = email.downcase
   end
-
-  def User.digest(string)
+  
+class << self
+  def digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
-  def User.new_token
+  def new_token
     SecureRandom.urlsafe_base64
   end
+end
 
   def remember
     self.remember_token = User.new_token
